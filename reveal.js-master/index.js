2cfdc93dcdf67d51033dec2011d7f4fbe716c381
IT210185 <113981420+IT210185@users.noreply.github.com>
2023-03-14 11:24:58 +0100

    Add files via upload

---
+++ reveal.js-master/index.js
@@ -0,0 +1,7 @@
+Reveal.initialize({
+    hash: true,
+    loop: true,
+
+    // Learn about plugins: https://revealjs.com/plugins/
+    plugins: [ RevealMarkdown, RevealHighlight, RevealNotes ]
+});
